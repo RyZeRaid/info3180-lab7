9cbb557e7221b5ddebf7595b22d42d87311a55a3
Onandi Skeen <onandiskeen@gmail.com>
2022-04-10 22:16:39 -0400

    Added upload route

--- app/views.py
+++ app/views.py
@@ -5,9 +5,14 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
+from crypt import methods
 from app import app
 from flask import render_template, request, jsonify, send_file
+from .forms import UploadForm
+from werkzeug.utils import secure_filename
 import os
+
+from app.forms import UploadForm
 
 
 ###
@@ -19,6 +24,21 @@ def index():
     return jsonify(message="This is the beginning of our API")
 
 
+
+@app.route('/api/upload', methods=["POST"])
+def upload():
+    form = UploadForm()
+
+    if request.method == "POST" and form.validate_on_submit:
+        description = form.description.data
+
+        photo = form.photo.data
+        photoname = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photoname))
+
+        return jsonify(message="File Upload Successful" , filename = photoname, description = description )
+    else:
+        return jsonify(errors = form_errors(form))
 ###
 # The functions below should be applicable to all Flask apps.
 ###
